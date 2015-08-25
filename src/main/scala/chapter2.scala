0d56fbd8f9af2e9ca9bb32ec2c107d7d6e6be154
Arya Irani <arya.irani@gmail.com>
2015-08-25 01:49:14 -0400

    added alterative versions of chapter2 exercises and refactored tests

--- src/main/scala/chapter2.scala
+++ src/main/scala/chapter2.scala
@@ -5,23 +5,57 @@ import annotation.tailrec
 object chapter2 {
 	def fib(n: Int): Int = {
 		require(n >= 1)
-		@tailrec def loop(x: Int, a1: Int, a2: Int): Int = x match {
-			case 1 => a1
-			case n => loop(n-1, a2, a2+a1)
-		}
-		loop(n,0,1)
+    // Generates a stream of Fibonacci numbers.
+    // `n` is the number of Fibonacci numbers to generate.
+    // `a1` and `a2` are the next two numbers in the stream.
+		@tailrec def fibHelper(n: Int, a1: Int, a2: Int): Int =
+      if (n == 1) a1
+      else fibHelper(n-1, a2, a1+a2)
+		fibHelper(n,0,1)
 	}
 
-	def isSorted[A](as: Vector[A], ordered: (A,A) => Boolean): Boolean = {
-		@tailrec def loop(idx: Int): Boolean = {
-			if (idx >= 0)
-			  ordered(as(idx), as(idx+1)) && loop(idx-1)
-		  else true
-		}
-		loop(as.length-2)
-	}
+	def fibIter(_n: Int) = {
+    require(_n >= 1)
+    var n = _n
+    var a = 0
+    var b = 1
+    while(n > 1) {
+      val temp = a
+      a = b
+      b = temp + a
+      n -= 1
+    }
 
-	def curry[A,B,C](f: (A,B) => C): A => (B => C) =
+    a
+  }
+
+  def fibIterator(n: Int): Int = {
+    require(n >= 1)
+    Iterator.iterate((0,1)) { case (a,b) => (b, a+b) }.map(_._1).drop(n-1).next()
+  }
+
+  def isSorted[A](v: Vector[A], ordered: (A,A) => Boolean): Boolean = {
+    @tailrec def loop(idx: Int): Boolean =
+      if (idx >= v.length - 1) true
+      else ordered(v(idx), v(idx+1)) && loop(idx+1)
+    loop(0)
+  }
+
+  def isSortedIter[A](v: Vector[A], ordered: (A,A) => Boolean): Boolean = {
+    var idx = 0
+    while (idx < v.length-1) {
+      if (!(ordered(v(idx), v(idx+1)))) return false
+      idx += 1
+    }
+    return true
+  }
+
+  def isSortedLib[A](v: Vector[A], ordered: (A,A) => Boolean): Boolean = {
+    0 to v.length - 2 forall { i => ordered(v(i), v(i+1)) }
+  }
+
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) =
 		a => f(a,_) // or a => b => f(a,b)
 
 	def uncurry[A,B,C](f: A => B => C): (A, B) => C =
--- src/test/scala/chapter2test.scala
+++ src/test/scala/chapter2test.scala
@@ -5,18 +5,31 @@ import org.scalacheck._ // libraryDependencies += "org.scalacheck" %% "scalachec
 object chapter2test extends Properties("chapter2") {
 	import chapter2._
 
-	val smallInteger = Gen.choose(1,100)
+	val smallInteger = Gen.choose(1,10)
 
-  property("fib") = Prop.forAll(smallInteger) { n =>
-  	fib(n) == (n match {
-  		case 1 => 0
-  		case 2 => 1
-  		case n => fib(n-1) + fib(n-2)
-  	}) // compare tail recursive version with simpler, non-tail-recursive version
-  }
+	testFib("fib", fib)
+  testFib("fibIter", fibIter)
+  testFib("fibIterator", fibIterator)
 
-  property("isSorted") = Prop.forAll { (as: Vector[Int]) =>
-  	val intLT: (Int,Int) => Boolean = _ <= _
-  	isSorted(as, intLT) == (as == as.sortWith(intLT)) // compare result against standard library function
-  }
+  testSort("isSorted", isSorted)
+  testSort("isSortedIter", isSortedIter)
+  testSort("isSortedLib", isSortedLib)
+
+  def testFib(name: String, testFib: Int => Int) =
+    property(name) = Prop.forAll(smallInteger) { n =>
+      def referenceFib: Int => Int = {
+        case 1 => 0
+        case 2 => 1
+        case n => referenceFib(n-1) + referenceFib(n-2)
+      }
+      testFib(n) == referenceFib(n)
+		}
+
+	def testSort(name: String, isSorted: (Vector[Int], (Int,Int) => Boolean) => Boolean) =
+		property(name) = Prop.forAll {
+			(as: Vector[Int]) =>
+				isSorted(as, _ <= _) == (as == as.sortWith(_ < _)) &&
+        isSorted(as.sortWith(_ < _), _ <= _)
+		} // a little confusing; method should take lteq, sort takes
+
 }
